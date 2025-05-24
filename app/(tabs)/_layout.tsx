95228be909b7647f57c72a837644a2bbca5d7b41
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2025-05-24 23:52:45 +0530

    ✨ Add tabs navigator

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,70 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform, StyleSheet, useWindowDimensions, View } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { ThemedText } from "@/components/ThemedText";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  const { width } = useWindowDimensions();
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+        tabBarBackground: () => {
+          return (
+            <View
+              style={{
+                backgroundColor: Colors[colorScheme ?? "light"].cardBackground,
+                width: "100%",
+                height: 80,
+              }}
+            />
+          );
+        },
+        tabBarStyle: {
+          borderTopWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+          borderWidth: 0,
+          position: "absolute",
+          bottom: 20,
+          width: width / 2,
+          maxWidth: 400,
+          // backgroundColor: "white",
+          borderRadius: 50,
+          height: 60,
+          overflow: "hidden",
+          marginLeft: width / 4,
+          paddingTop: 3,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Workouts",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="list.triangle" color={color} />
+          ),
         }}
       />
     </Tabs>
